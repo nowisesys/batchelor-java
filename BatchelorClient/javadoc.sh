7471d5e74c1051c74a1592769f6aa6ff9880ae07
Anders Lövgren <andlov@nowise.se>
2009-05-06 02:45:15 +0000

    Added simple shell script for creating the javadoc API docs.

---
+++ BatchelorClient/javadoc.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# Generate javadoc for the client library API.
+#
+# Author: Anders Lövgren
+# Date:   2009-05-06
+
+ANT_COMMAND="ant"
+ANT_TARGET="jar"
+
+JAVADOC="javadoc"
+FOA_JAR="../../FOA/dist/foa-java-1.0.1.jar"
+ENCODING="utf8"
+
+DESTDIR="dist/api"
+SRCDIR="../CommonLibrary/src src build/generated/wsimport/client"
+
+JARS=${FOA_JAR}
+
+if ! [ -d dist ]; then
+  ${ANT_COMMAND} ${ANT_TARGET}
+fi
+
+${JAVADOC} -classpath ${JARS} -d ${DESTDIR} -encoding ${ENCODING} $(find ${SRCDIR} -type f | egrep '\.java$')
