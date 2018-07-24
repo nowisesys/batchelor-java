76ea81cc67e9aba6e5abfb089b9d20f009862dc4
Anders Lövgren <andlov@nowise.se>
2018-07-24 23:28:05 +0000

    Clean dist directory before creating binary package.

--- package.sh
+++ package.sh
@@ -6,9 +6,13 @@
 # Date:   2018-07-25
 
 path=dist
-file=batchelor-java-1.0.5.zip
+file=batchelor-java-1.0.6.zip
 
-mkdir dist/lib
+if [ -d dist ]; then
+  rm -rf dist/*
+fi
+
+mkdir -p dist/lib
 
 cp -a explorer/dist/*.jar dist
 cp -a explorer/dist/lib/* dist/lib
@@ -18,6 +22,6 @@ cp -a common/dist/*.jar dist
 cp -a client/dist/*.jar dist
 cp -a client/dist/lib/* dist/lib
 
-( cd dist && zip -r batchelor-java-1.0.5.zip * )
+( cd dist && zip -r $file * )
 
-echo "Created binary distribution file $path/$name"
+echo "Created binary distribution file $path/$file"
